93e385572a80334da98a3cf60b36a0c9f4b9bca3
José Gonzalo Domínguez Moreno <72448893+gonzalodom11@users.noreply.github.com>
2021-12-10 23:08:12 +0100

    Update LosAngeles_TEST.py

--- src/LosAngeles_TEST.py
+++ src/LosAngeles_TEST.py
@@ -16,7 +16,16 @@ def leer_censusTEST():
 	print(census[0:2])
 	print(census[-3:-1])
 	print(len(census))
+	
+def sort_by_location_TEST(census,location):
+    res=sort_by_location(census,location)   
+    print(res)
 
+def population_hood_TEST(census):
+    print(population_hood(census))
+
+def topx_population_TEST(census):
+    print(topx_population(census))
 
 
 leer_censusTEST();
