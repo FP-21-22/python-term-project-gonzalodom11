b992fb8efc08af7301b1767094a5a355dd5e11e9
José Gonzalo Domínguez Moreno <72448893+gonzalodom11@users.noreply.github.com>
2021-12-10 23:07:11 +0100

    Update LosAngeles.py

--- src/LosAngeles.py
+++ src/LosAngeles.py
@@ -34,6 +34,55 @@ def leer_census(file):
     
     return res
 
+def neighborhoods(census):
+    '''
+     It gets an ordered list of one of the fields with no repetition
+    '''
+    list1=[]
+   
+    
+    for c in census:
+        if c.hood in list1:
+            pass
+        else:
+            list1.append(c.hood)
+        
+    return sorted(list1)
+
+def sort_by_location(census,location):
+    '''
+    It gets a list of a field (hoods) in which 
+    other field have to hold a condition (location). Location to try: beach, mountain and city
+    '''
+    aux=[]
+    total_hoods=neighborhoods(census)
+    for c in census:
+        if c.location==location and c.hood not in aux:
+            aux.append(c.hood)
+    print("Number of neighborhoods in the",location+":",len(aux),"/",len(total_hoods))
+    print("")       
+    return sorted(aux)
+
+def population_hood(census):
+    
+    '''
+    It returns a dictionary which link two fields: for every neighborhood it sums the population 
+    of every zip code  and the RESULT is a dictionary in which the key is the neighborhood and the value is the total population 
+    of it
+    '''
+    aux=dict()
+    hoods=neighborhoods(census)
+    
+    for h in hoods:
+        population=0
+        for c in census:
+            if c.hood==h:
+                population+=int(c.population)
+        
+        aux[h]=population
+        
+    return aux
+
 
 
 
